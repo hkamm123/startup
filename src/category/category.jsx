1781b627d244f05fa652c9fcd1078fc8e195c3ff
Hyrum Kammerman <kammerman.h@gmail.com>
2025-10-08 14:54:57 -0600

    (reactP1) create boilerplate components to be routed to

---
+++ src/category/category.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Category() {
+  return (
+    <main className="container-fluid bg-secondary text-center">
+      <div>category displayed here</div>
+    </main>
+  );
+}
---
+++ src/expense/expense.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Expense() {
+  return (
+    <main className="container-fluid bg-secondary text-center">
+      <div>expense displayed here</div>
+    </main>
+  );
+}
