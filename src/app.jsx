1d486e02b84741a5a29e32c4782f568531842c43
Hyrum Kammerman <kammerman.h@gmail.com>
2025-10-07 20:42:14 -0600

    (reactP1) add header and footer to app component

--- src/app.jsx
+++ src/app.jsx
@@ -3,5 +3,25 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
 export default function App() {
-  return <div className="app bg-dark text-light">App will display here</div>;
+  return <div className="body bg-dark text-light">
+    <header>
+        <h1>NoNonCents</h1>
+        <nav>
+            <menu>
+            <li><a href="index.html">Home</a></li>
+            <li><a href="budget.html">My Budget</a></li>
+            <li><a href="expense.html">Add Expense</a></li>
+            </menu>
+        </nav>
+    </header>
+
+    <main>
+        Main components will go here
+    </main>
+
+    <footer>
+        <p>Created by Hyrum Kammerman</p>
+        <a href="https://github.com/hkamm123/startup">GitHub</a>
+    </footer>
+  </div>;
 }
