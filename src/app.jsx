21438178895b1d4849991e539e4bd124da4b4bed
Hyrum Kammerman <kammerman.h@gmail.com>
2025-10-08 15:10:39 -0600

    (reactP1) convert navigation to React routing

--- src/app.jsx
+++ src/app.jsx
@@ -2,26 +2,43 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import { Login } from './login/login';
+import { Budget } from './budget/budget';
+import { Category } from './category/category';
+import { Expense } from './expense/expense';
+
 export default function App() {
-  return <div className="body bg-dark text-light">
-    <header>
-        <h1>NoNonCents</h1>
-        <nav>
-            <menu>
-            <li><a href="index.html">Home</a></li>
-            <li><a href="budget.html">My Budget</a></li>
-            <li><a href="expense.html">Add Expense</a></li>
-            </menu>
-        </nav>
-    </header>
+  return (
+  <BrowserRouter>
+    <div className="body bg-dark text-light">
+      <header>
+          <h1>NoNonCents</h1>
+          <nav>
+              <menu>
+              <li><NavLink className="nav-link" to="/">Home</NavLink></li>
+              <li><NavLink className="nav-link" to="budget">My Budget</NavLink></li>
+              <li><NavLink className="nav-link" to="expense">Add Expense</NavLink></li>
+              </menu>
+          </nav>
+      </header>
 
-    <main>
-        Main components will go here
-    </main>
+      <Routes>
+        <Route path='/' element={<Login />} exact />
+        <Route path='/budget' element={<Budget />} />
+        <Route path='/category' element={<Category />} />
+        <Route path='/expense' element={<Expense />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
 
-    <footer>
-        <p>Created by Hyrum Kammerman</p>
-        <a href="https://github.com/hkamm123/startup">GitHub</a>
-    </footer>
-  </div>;
+      <footer>
+          <p>Created by Hyrum Kammerman</p>
+          <a href="https://github.com/hkamm123/startup">GitHub</a>
+      </footer>
+    </div>
+  </BrowserRouter>);
+}
+
+function NotFound() {
+  return <main className="container-fluid bg-secondary text-center">404: Page not found.</main>;
 }
