72eed856654b079e1c10c4042b8ebc2b8739932a
Hyrum Kammerman <kammerman.h@gmail.com>
2025-11-11 20:28:38 -0700

    (db) add mongo connection capability to backend

--- service/index.js
+++ service/index.js
@@ -1,3 +1,22 @@
+const { MongoClient } = require('mongodb');
+const config = require('./dbConfig.json');
+
+const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
+const client = new MongoClient(url);
+const db = client.db('startup');
+const userCollection = db.collection('user');
+const budgetCollection = db.collection('budget');
+
+(async function testConnection() {
+  try {
+    await db.command({ ping: 1 });
+    console.log(`Connected to database`);
+  } catch (ex) {
+    console.log(`Unable to connect to database with ${url} because ${ex.message}`);
+    process.exit(1);
+  }
+})();
+
 const express = require('express');
 const app = express();
 
